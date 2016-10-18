4044a69201f8f8cfa1042bc0abb76c9a74d53715
Phoenix Bishea <phoenixb@utexas.edu>
2016-10-17 20:46:48 -0500

    Rearrange files, add .travis.yml that builds on push, cleanup existing files, and update .gitignore

--- application.py
+++ application.py
@@ -1,46 +1,15 @@
 from flask import Flask, render_template
 
-# print a nice greeting.
-
-#
-# def say_hello(username = "World"):
-#     return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-# header_text = '''
-#     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-# instructions = '''
-#     <p><em>Hint</em>: This is a RESTful web service! Append a username
-#     to the URL (for example: <code>/Thelonious</code>) to say hello to
-#     someone specific.</p>\n'''
-# home_link = '<p><a href="/">Back</a></p>\n'
-# footer_text = '</body>\n</html>'
-
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
-# application.add_url_rule('/', 'index', (lambda: header_text +
-#     say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-# application.add_url_rule('/<username>', 'hello', (lambda username:
-#     header_text + say_hello(username) + home_link + footer_text))
-
-
-@application.route('/test')
-def test_page():
-    return "<p> This page is a test </p>"
-
-
+# Return the home page.
 @application.route('/')
 def index():
     return render_template('index.html')
 
-# run the app.
+# Run the application.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    application.run(debug=True)
