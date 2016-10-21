2f461c599494beb2eed047a76aaab02d22e1e4c9
Phoenix Bishea <phoenixb@utexas.edu>
2016-10-20 20:49:30 -0500

    Add two tests for all application.py functions and remove unused data_summary.html

--- application_test.py
+++ application_test.py
@@ -2,6 +2,7 @@
 Module for testing the main application
 """
 import unittest
+from bs4 import BeautifulSoup
 from application import application
 
 
@@ -9,17 +10,62 @@ class ApplicationTest(unittest.TestCase):
     """ Tests for application """
 
     def setUp(self):
+        self.routes = [
+            '/', 
+            '/states/texas',
+            '/states/alaska',
+            '/states/california',
+            '/elections/general_election_2016',
+            '/elections/texas_23_district_2016',
+            '/elections/california_12_district_2016',
+            '/candidates/gary_johnson',
+            '/candidates/ted_cruz',
+            '/candidates/nancy_pelosi',
+            '/parties/libertarian',
+            '/parties/democratic',
+            '/parties/republican',
+            '/states',
+            '/elections',
+            '/candidates',
+            '/parties',
+            '/about']
+
+        self.html_docs = [
+            'index.html',
+            'states_texas.html',
+            'states_alaska.html',
+            'states_california.html',
+            'elections_general_election_2016.html',
+            'elections_texas_23_district_2016.html',
+            'elections_california_12_district_2016.html',
+            'candidates_gary_johnson.html',
+            'candidates_ted_cruz.html',
+            'candidates_nancy_pelosi.html',
+            'parties_libertarian.html',
+            'parties_democratic.html',
+            'parties_republican.html',
+            'states_model.html',
+            'elections_model.html',
+            'candidates_model.html',
+            'parties_model.html',
+            'about.html']
+
         self.app = application.test_client()
         self.app.testing = True
 
     def test_home_status(self):
         """ Test proper status code return """
-        self.assertEqual(self.app.get('/').status_code, 200)
+        for route in self.routes:
+            self.assertEqual(self.app.get(route).status_code, 200)
 
     def test_home_data(self):
         """ Test opening webpages """
-        page = open('templates/index.html')
-        self.assertEqual(page.read(), self.app.get('/').data.decode('utf-8'))
+        for index in range(len(self.routes)):
+            fetched_page = self.app.get(self.routes[index])
+            route = BeautifulSoup(fetched_page.data.decode(), 'html.parser')
+            html_file = open('templates/' + self.html_docs[index])
+            page = BeautifulSoup(html_file, 'html.parser')
+        self.assertEqual(route.title.string, page.find('title').string)
 
     def tearDown(self):
         pass
