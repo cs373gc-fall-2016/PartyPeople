1a5a3fe398dd7cb757f710de3fd7a79f412bb06c
GreatImNate <nathan.seegmiller@utexas.edu>
2016-10-26 18:54:13 -0500

    better model representation with multiple and intermediate tables. Relationships are cleaner and no circular relationships, tests that are written now testing insertion into the table all pass on my local machine, more tests still needed to verify that the relationships work properly

--- app/model_test.py
+++ app/model_test.py
@@ -2,9 +2,11 @@
 Module for testing models
 """
 # pylint: disable=invalid-name,line-too-long,no-member,locally-disabled
-
+# todo : test multi relationships [State(Many), Party(One)], [State(One), Politician(Many)]
+# todo : [Election(One), Party(Many)], [Election(One), Politician(Many)]
+# todo : [Politician(Many), Party(One)]
 import unittest
-from models import State, Party, Candidate, database #, Candidate, Election, Party, database
+from models import State, Party, Candidate, database, Election, ElectoralCollege, PartiesInvolved
 from datetime import datetime
 
 
@@ -12,67 +14,89 @@ class ModelTest(unittest.TestCase):
     """ Class for testing models """
 
     def setUp(self):
-        """Set up the database and the Models for testing"""
+        """Set up the database and the Models for testing
+        Will have multiple of each type of model to be placed in the tables, to test different types of relationships
+        """
         database.create_all()
-        self.candidate = Candidate(name="Bill") #, dob=datetime(1900, 2, 3, 4, 12, 30, 00), job="candidate",
-                                   # party="Democratic", poll=50.0, contact="bill@bill.org")
-        # self.election = Election(ename="general", date=datetime(
-        #     2016, 11, 8, 12, 0, 0), level='state')
-        # self.election2 = Election(name="local", date=datetime(2016, 11, 8, 12, 0, 0), level='state',
-        #                           location="texas", politicians=["nobody", "anybody"])
-        self.state = State(name='alaska', capital='juneau', population=123456)
-        self.party = Party(name="Republican") #, description="Republican Party", politicians=["Trump", "Bush"],
-        #                    state=["Texas", "Mississippi"], hq="DC", leader="Mr. President")
+        self.candidate_1 = Candidate(name="Candidate_1", dob=None, job='politician', contact='candidate1@us.gov', poll=50.0)
+        self.candidate_2 = Candidate(name="Candidate_2", dob=None, job='politician', contact='candidate2@us.gov', poll=75.0)
+        self.candidate_3 = Candidate(name="Candidate_3", dob=None, job='governor', contact='candidate3@us.gov', poll=50.0)
+        self.election_1 = Election(name="general", date=None, level='federal')
+        self.election_2 = Election(name="local_1", date=None, level='local_1')
+        self.election_3 = Election(name="local_2", date=None, level='local_2')
+        self.election_3 = Election(name="state", date=None, level='state')
+
+        self.state_1 = State(name='State_1', capital='Capital_1', population=1000)
+        self.state_2 = State(name='State_2', capital='Capital_2', population=2000)
+        self.state_3 = State(name='State_3', capital='Capital_3', population=1000)
+
+        self.party_1 = Party(name="Party_1", description='party people', hq='hq_1', leader='leader_1')
+        self.party_2 = Party(name="Party_2", description='people party', hq='hq_2', leader='leader_2')
+        self.party_3 = Party(name="Party_3", description='people people', hq='hq_1', leader='leader_3')
 
     def tearDown(self):
         """Drop all the tables so on subsequent tests it doesn't complain that elements already exist"""
         database.session.close()
         database.reflect()
         database.drop_all()
+        # Using the drop all command will give an 'index out of bounds error'
+        # Without the drop test will fail because it might not be testing the same table that was just placed in
+        # Can drop when the table is just freshly created
 
     def test_state(self):
         """ Test state model """
-        state = self.state
-        state.candidates = self.candidate
+        print('Running State Model Test')
+        state = self.state_1
         # state.party_affiliation = self.party
         database.session.add(state)
         database.session.commit()
         query = State.query.all()
         print(query)
-
         self.assertEqual(query[0].name, state.name)
-        print(query[0].candidates)
-        print(self.candidate)
-    # def test_Candidate(self):
-    #     """Test the Candidate model"""
-    #     candidate = self.candidate
-    #     database.session.add(candidate)
-    #     database.session.commit()
-    #     query = Candidate.query.all()
-    #     self.assertEqual(query[0], candidate)
-    #     print(query[0])
-    #     print(query[0].elections)
-    #
-    # def test_election(self):
-    #     """Test the Election Model"""
-    #     election = self.election
-    #     database.session.add(election)
-    #     database.session.commit()
-    #     query = Election.query.all()
-    #     self.assertEqual(query[0], election)
-    #
-    # def test_party(self):
-    #     """Test the party Model"""
-    #     party = self.party
-    #     database.session.add(party)
-    #     database.session.commit()
-    #     query = Party.query.all()
-    #     self.assertEqual(query[0], party)
-    #
-    # def test_relation(self):
-    #     """Test the relationship between two models"""
-    #     self.candidate.elections = [self.election, self.election2]
 
+    def test_candidate(self):
+        """Test the Candidate model"""
+        print('Running Candidate Model Test')
+        candidate = self.candidate_1
+        database.session.add(candidate)
+        database.session.commit()
+        query = Candidate.query.all()
+        print(query)
+        self.assertEqual(query[0].name, candidate.name)
+
+    def test_election(self):
+        """Test the Election Model"""
+        print('Running the Election Model Test')
+        election = self.election_1
+        database.session.add(election)
+        database.session.commit()
+        query = Election.query.all()
+        self.assertEqual(query[0], election)
+
+    def test_party(self):
+        """Test the party Model"""
+        print('Running the Party Model Test')
+        party = self.party_1
+        database.session.add(party)
+        database.session.commit()
+        query = Party.query.all()
+        self.assertEqual(query[0], party)
+
+    def test_electoral_college(self):
+        """
+        :return:
+        """
+        print("Running the Electoral College Test")
+        relation = ElectoralCollege()
+        relation.party = self.party_1
+        relation.states = self.state_1
+        database.session.add(relation)
+        database.session.commit()
+        query = ElectoralCollege.query.all()
+        print(query)
+
+    def test_parties_involved(self):
+        pass
 
 # if __name__ == '__main__':
 #     # unittest.main()
--- app/models.py
+++ app/models.py
@@ -8,102 +8,130 @@ application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ppdb'
 
 database = SQLAlchemy(application)
 
+"""
+Questions and Concerns about DB:
+Should we have default values in the tables? Some models don't really have things that fit
+    for example the general election is in the election model, and initially our plan was to have states as a part of
+    the election model. In the general election though all the states are participating. How would this be shown in a
+    database? Would there be a value in the state table that is a representation for all the states, so that the
+    general election could have a value to map to, either as a foreign key, or in an intermediate table?
+What relationships are worth having as foreign keys, and which are better suited for another table? What values in each
+    model are inherent to that model? For instance party is inherent to Candidate, because ever politician in the United
+    States is considered to be part of some political party, even if it is 'independent'
+If a call is state1 = State() and state2 = State() will these be in the same table? They should be.
+"""
+
 
 class Candidate(database.Model):
     """ Candidate Model class """
     __tablename__ = "candidate"
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String)
-#     dob = database.Column(database.DateTime)
-#     job = database.Column(database.String)
-#     election_id = database.Column(
-#         database.Integer, database.ForeignKey('election.id'))
-#     state_id = database.Column(
-#         database.Integer, database.ForeignKey('state.id', use_alter=True, name=''))
-#     states = database.relationship(
-#         'State', backref='candidate', foreign_keys=[state_id])
-#     party_id = database.Column(
-#         database.Integer, database.ForeignKey('party.id'))
-#     party = database.relationship(
-#         'Party', backref='candidate', foreign_keys=[party_id])
-#     elections = database.relationship(
-#         'Election', backref='candidate_election', foreign_keys=[election_id])
-#     poll = database.Column(database.Float)
-#     contact = database.Column(database.String)
-#
-#     def __repr__(self):
-#         return '<Candidate %r %r %r %r %r %r %r %r>' %\
-#                (self.name, self.dob, self.job, self.party, self.state,
-#                 self.elections, self.poll, self.contact)
-#
-#
+    dob = database.Column(database.DateTime)
+    job = database.Column(database.String)
+    contact = database.Column(database.String)
+    poll = database.Column(database.Float)
+    election_id = database.Column(
+        database.Integer, database.ForeignKey('election.id'))
+    elections = database.relationship(
+        'Election', backref='candidate_election', foreign_keys=[election_id])
+    state_id = database.Column(
+        database.Integer, database.ForeignKey('state.id'))
+    states = database.relationship(
+        'State', backref='candidate', foreign_keys=[state_id])
+    party_id = database.Column(
+        database.Integer, database.ForeignKey('party.id'))
+    party = database.relationship(
+        'Party', backref='candidate', foreign_keys=[party_id])
+
+    def __repr__(self):
+        return '<Candidate %r %r %r %r %r>' %\
+               (self.name, self.dob, self.job, self.poll, self.contact)
+
+
 class Election(database.Model):
     """ Election Model class """
     __tablename__ = "election"
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String)
-#     date = database.Column(database.DateTime)
-#     level = database.Column(database.String)
-#     candidate_id = database.Column(
-#         database.Integer, database.ForeignKey('candidate.id'))
-#     state_id = database.Column(
-#         database.Integer, database.ForeignKey('state.id'))
-#     party_id = database.Column(
-#         database.Integer, database.ForeignKey('party.id'))
-#     politicians = database.relationship(
-#         'Candidate', backref='election', foreign_keys=[candidate_id])
-#     state = database.relationship(
-#         'State', backref='election_state', foreign_keys=[state_id])
-#     party = database.relationship(
-#         'Party', backref='election', foreign_keys=[party_id])
-#
-#     def __repr__(self):
-#         return '<Election %r %r %r %r %r>' % (self.name, self.date, self.level, self.location, self.politicians)
-#
-#
+    date = database.Column(database.DateTime)
+    level = database.Column(database.String)
+
+    def __repr__(self):
+        return '<Election %r %r %r>' % (self.name, self.date, self.level)
+
+
 class Party(database.Model):
     """ Party Model class """
     __tablename__ = "party"
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String)
-#     description = database.Column(database.String)
-#     candidate_id = database.Column(
-#         database.Integer, database.ForeignKey('candidate.id'))
-#     state_id = database.Column(
-#         database.Integer, database.ForeignKey('state.id'))
-#     politicians = database.relationship(
-#         'Candidate', backref='party', foreign_keys=[candidate_id])
-#     state = database.relationship(
-#         'State', backref='party_state', foreign_keys=[state_id])
-#     hq = database.Column(database.String)
-#     leader = database.Column(database.String)
-#
-#     def __repr__(self):
-#         return '<Party %r %r %r %r>' % (self.name, self.description, self.state, self.leader)
-#
+    description = database.Column(database.String)
+    hq = database.Column(database.String)
+    leader = database.Column(database.String)
+
+    def __repr__(self):
+        return '<Party %r %r %r>' % (self.name, self.description, self.leader)
+
 
 class State(database.Model):
-    """ State Model class """
+    """ State Model class
+    :var id = id of the state in the datbase
+    :var name = name of the state
+    :var capital Current capital of the state
+    :var population total population in the state as of last census
+    :var governor Current Governor of the state
+    """
     __tablename__ = "state"
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String)
     capital = database.Column(database.String)
     population = database.Column(database.Integer)
     governor = database.Column(database.String)
-    candidate_id = database.Column(
-        database.Integer, database.ForeignKey('candidate.id'))
-    candidates = database.relationship(
-        'Candidate', backref='state', foreign_keys=[candidate_id])
-    # election_id = database.Column(
-    #     database.Integer, database.ForeignKey('election.id'))
-    # elections = database.relationship(
-    #     'Election', backref='state_election', foreign_keys=[])
-    # party_id = database.Column(
-    #     database.Integer, database.ForeignKey('party.id'))
-    # party_affiliation = database.relationship(
-    #     'Party', backref='state_party')
 
     def __repr__(self):
         return '<State %r %r %r %r>' %\
                (self.name, self.capital, self.population,
                 self.governor)
+
+
+class ElectoralCollege(database.Model):
+    """
+    :Relations = State and Party
+    Uses = Determine which party controls which state
+    """
+    __tablename__ = 'electoral_college'
+    id = database.Column(database.Integer, primary_key=True)
+    party_id = database.Column(
+        database.Integer, database.ForeignKey('party.id'))
+    party = database.relationship(
+        'Party', backref='electoral', foreign_keys=[party_id])
+    state_id = database.Column(
+        database.Integer, database.ForeignKey('state.id'))
+    states = database.relationship(
+        'State', backref='electoral', foreign_keys=[state_id])
+
+    def __repr__(self):
+        return '<ElectoralCollege State %r Party %r>' % (self.states, self.party)
+
+
+class PartiesInvolved(database.Model):
+    """
+    Relation (Party, Election) many different parties involved in a single election
+    """
+    __tablename__ = 'parties_involved'
+    id = database.Column(database.Integer, primary_key=True)
+    party_id = database.Column(
+        database.Integer, database.ForeignKey('party.id'))
+    party = database.relationship(
+        'Party', backref='parties_involved', foreign_keys=[party_id])
+    election_id = database.Column(
+            database.Integer, database.ForeignKey('election.id'))
+    elections = database.relationship(
+       'Election', backref='parties_involved', foreign_keys=[election_id])
+
+    def __repr__(self):
+        return '<Party %r Election %r>' % (self.party, self.elections)
+"""
+Have more intermediate tables that will relate the main four models
+"""
