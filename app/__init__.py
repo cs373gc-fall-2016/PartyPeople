d96046f0cb942d806ffbbd684f3f8b63809694ac
Phoenix Bishea <phoenixb@utexas.edu>
2016-11-04 17:44:00 -0500

    remove unneeded files and update .gitignore

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# application = Flask(__name__)
-# application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://partypeople:sweatypeople@' \
-#                                                 'partypeople-postgresql.cldmkovirzyt.us-west-2.rds.amazonaws.com:5432/partypeople_database'
-# # application.config
-# database = SQLAlchemy(application)
