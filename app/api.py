108a642869451341154f2655afa3b34d8a0579a8
GreatImNate <nathan.seegmiller@utexas.edu>
2016-11-11 12:30:47 -0600

    Disabled pagination on the API results

--- app/api.py
+++ app/api.py
@@ -6,9 +6,9 @@ import app.models as models
 def create_api_endpoints(app):
 	""" Create API endpoints available at /api/<tablename> by default """
 	manager = APIManager(app, flask_sqlalchemy_db=models.database)
-	manager.create_api(models.State)
-	manager.create_api(models.Party)
-	manager.create_api(models.Candidate)
-	manager.create_api(models.Election)
-	manager.create_api(models.ElectoralCollege)
-	manager.create_api(models.PartiesInvolved)
+	manager.create_api(models.State, results_per_page=0)
+	manager.create_api(models.Party, results_per_page=0)
+	manager.create_api(models.Candidate, results_per_page=0)
+	manager.create_api(models.Election, results_per_page=0)
+	manager.create_api(models.ElectoralCollege, results_per_page=0)
+	manager.create_api(models.PartiesInvolved, results_per_page=0)
