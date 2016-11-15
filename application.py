5ec26123e3b4bd80b5eae5bd93435d8204351b2d
GreatImNate <nathan.seegmiller@utexas.edu>
2016-11-15 12:42:00 -0600

    Trying to resolver error Target WSGI script '/opt/python/current/app/application.py' does not contain WSGI application 'application' by moving the setter of application outside of main

--- application.py
+++ application.py
@@ -4,27 +4,27 @@ from app import searchdb, query
 from app.models import Candidate, Election, Party, State, ElectoralCollege, PartiesInvolved, ElectionsToState
 
 def create_app():
-    application = Flask(__name__)
-    application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://partypeople:sweatypeople@partypeople-postgresql.cldmkovirzyt.us-west-2.rds.amazonaws.com:5432/partypeople_database'
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://partypeople:sweatypeople@partypeople-postgresql.cldmkovirzyt.us-west-2.rds.amazonaws.com:5432/partypeople_database'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ppdb'
-    application.debug = True
+    app.debug = True
 
     from app.models import database
-    database.init_app(application)
+    database.init_app(app)
 
     from app.api import create_api_endpoints
-    create_api_endpoints(application)
+    create_api_endpoints(app)
 
     from app.views import frontend
-    application.register_blueprint(frontend)
+    app.register_blueprint(frontend)
 
     @app.route("/api/s_and")
-    def s_and():
+    def  s_and():
         term = request.args.get("term")
         return searchdb.search_and(term)
 
     @app.route("/api/s_or")
-    def s_or():
+    def  s_or():
         term = request.args.get("term")
         return searchdb.search_or(term)
 
@@ -45,8 +45,10 @@ def create_app():
         return query.query_state()
 
 
-    return application
+    return app
 
 application = create_app()
+
 if __name__ == "__main__":
+    # app = create_app()
     application.run()
